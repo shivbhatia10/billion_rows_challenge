6343848cc6dbe24073cb12483bfc2dfacd2e65a0
Shiv Bhatia <shivbhatia10@gmail.com>
2026-09-07 17:40:25 +0100

    first working solution, 224.66s

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::fmt::Write;
 use std::{
-    collections::HashMap,
+    collections::BTreeMap,
     fs::File,
     io::{self, BufRead, BufReader},
 };
@@ -11,22 +12,66 @@ pub fn main() -> Result<(), io::Error> {
     let mut buf_reader = BufReader::with_capacity(1 << 20, file);
     let mut buffer = String::new();
 
-    let accs: HashMap<String, (f32, f32, f32, u32)> = HashMap::new();
+    // (min, total, max, count)
+    let mut accs: BTreeMap<String, (i16, i64, i16, u32)> = BTreeMap::new();
 
-    for i in 0..1_000_000_000 {
+    for _ in 0..1_000_000_000 {
+        buffer.clear();
         buf_reader.read_line(&mut buffer)?;
         let Some((key, value)) = buffer.split_once(";") else {
             continue;
         };
-        let value: f32 = value.trim().parse().map_err(|err| io::Error::other(err))?;
+        let value = parse_val(value)?;
 
-        let (min, sum, max, count) = accs.get(&key).unwrap_or((0.0, 0.0, 0.0, 0));
-
-        buffer.clear();
-        if i % 1_000_000 == 0 {
-            println!("{i}");
+        match accs.get_mut(key) {
+            Some(e) => {
+                e.0 = e.0.min(value);
+                e.1 += value as i64;
+                e.2 = e.2.max(value);
+                e.3 += 1;
+            }
+            None => {
+                accs.insert(key.to_string(), (value, value as i64, value, 1));
+            }
         }
     }
 
+    let mut out = String::with_capacity(accs.len() * 40);
+    out.push('{');
+    for (i, (name, (min, total, max, count))) in accs.iter().enumerate() {
+        if i > 0 {
+            out.push_str(", ");
+        }
+        let mean = *total as f64 / *count as f64 / 10.0;
+        write!(
+            out,
+            "{name}={:.1}/{mean:.1}/{:.1}",
+            *min as f64 / 10.0,
+            *max as f64 / 10.0
+        )
+        .unwrap();
+    }
+    out.push_str("}\n");
+    print!("{out}");
+
     Ok(())
+}
+
+fn parse_val(val: &str) -> Result<i16, io::Error> {
+    let s = val.trim_end();
+    let (neg, s) = match s.strip_prefix('-') {
+        Some(rest) => (true, rest),
+        None => (false, s),
+    };
+    let (whole, frac) = s
+        .split_once('.')
+        .ok_or(io::Error::other("Could not split {s}"))?;
+    let v = whole
+        .parse::<i16>()
+        .map_err(|e| io::Error::other(format!("Could not parse whole {e:?}")))?
+        * 10
+        + frac
+            .parse::<i16>()
+            .map_err(|e| io::Error::other(format!("Could not parse frac {e:?}")))?;
+    if neg { Ok(-v) } else { Ok(v) }
 }
