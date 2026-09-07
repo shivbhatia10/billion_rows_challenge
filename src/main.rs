b8a04e5ec21e6dd21658809b2e2e00b48c321144
Shiv Bhatia <shivbhatia10@gmail.com>
2026-09-07 16:42:56 +0100

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{self, BufRead, BufReader},
 };
@@ -7,16 +8,25 @@ const FILE_NAME: &'static str = "measurements.txt";
 
 pub fn main() -> Result<(), io::Error> {
     let file = File::open(FILE_NAME)?;
-    let mut buf_reader = BufReader::new(file);
+    let mut buf_reader = BufReader::with_capacity(1 << 20, file);
     let mut buffer = String::new();
 
-    for _ in 0..1_000_000_000 {
-        buf_reader.read_line(&mut buffer)?;
+    let accs: HashMap<String, (f32, f32, f32, u32)> = HashMap::new();
 
-        print!("{}", &buffer);
+    for i in 0..1_000_000_000 {
+        buf_reader.read_line(&mut buffer)?;
+        let Some((key, value)) = buffer.split_once(";") else {
+            continue;
+        };
+        let value: f32 = value.trim().parse().map_err(|err| io::Error::other(err))?;
+
+        let (min, sum, max, count) = accs.get(&key).unwrap_or((0.0, 0.0, 0.0, 0));
+
         buffer.clear();
+        if i % 1_000_000 == 0 {
+            println!("{i}");
+        }
     }
 
     Ok(())
 }
-
